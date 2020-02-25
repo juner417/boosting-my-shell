6463c0473b2bd02b8fe771fe60ee1902a8ee3d03
gonz.son <gonz@MacBook-Pro.local>
2020-02-25 14:23:02 +0900

    add script for install pkgs

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,3 +11,23 @@
 # copy .tmux.conf, .bash_profile
 # add alias
 # addons - proxy, etc..
+
+OS=x$(lsb_release -i | cut -d":" -f2 | tr -d "\t" | tr '[:upper:]' '[:lower:]')
+
+if [[ ${OS} == 'xubuntu' ]]; then
+  pkgmgr='apt-get'
+  run='install'
+  opt='--no-install-recommends -y'
+else if [[ ${OS} == 'xcentos' ]]; then
+  pkgmgr='yum'
+  run='install'
+  opt=''
+else
+  echo "${OS} is not supported"
+fi
+
+pkgs='ca-certificates git make jq nmap curl curl uuid-runtime br'
+
+sudo ${pkgmgr} update
+sudo ${pkgmgr} install ${opt} ${pkgs}
+
